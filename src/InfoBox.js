3aad049ea7c52a5bf825f3123591cd3d7afd4c0b
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-08-31 20:33:54 +0000

    Define the content of the InfoBox component

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
-function InfoBox() {
-  return <div></div>;
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+        <h2 className="infoxBox__cases">{cases}</h2>
+        <Typography className="infoBox__total">{total} Total</Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
