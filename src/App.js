5f3cb9a2f34c223b63a6eda6db754442357eb573
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-08-31 20:19:10 +0000

    Map country selected to dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FormControl, Select, MenuItem } from '@material-ui/core';
+import {
+  FormControl,
+  Select,
+  option,
+  Button,
+  InputLabel,
+  Input,
+  FormHelperText,
+  MenuItem,
+} from '@material-ui/core';
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState(['USA', 'Senegal', 'France']);
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
   useEffect(() => {
     const getCountriesData = async () => {
       axios.get('https://disease.sh/v3/covid-19/countries').then((response) => {
@@ -24,7 +34,8 @@ function App() {
       <div className="app__header">
         <h1>COVID 19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
