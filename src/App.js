c91ad6d0089685a7f02652b61ebaac3d80256edb
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-08-31 22:03:02 +0000

    Fetch automatically the worldwide cases on app load

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => setCountryInfo(data));
+  }, []);
+  useEffect(() => {
     const getCountriesData = async () => {
       axios.get('https://disease.sh/v3/covid-19/countries').then((response) => {
         const { data } = response;
