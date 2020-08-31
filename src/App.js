94c19df42b62607b1c5d7ea1fa93c786178ef445
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-08-31 21:15:26 +0000

    Move the previous code to the app__left div

--- src/App.js
+++ src/App.js
@@ -27,24 +27,30 @@ function App() {
     setCountry(countryCode);
   };
   return (
-    <div className="app">
-      <div className="app__header">
-        <h1>COVID 19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onChange={onCountryChange}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+    <div className="app__left">
+      <div className="app">
+        <div className="app__header">
+          <h1>COVID 19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus cases" cases={123} total={2000} />
+          <InfoBox title="Recovered" total={3000} cases={1256} />
+          <InfoBox title="Deaths" cases={3679} total={30} />
+        </div>
+        <Map />
       </div>
-      <div className="app__stats">
-        <InfoBox title="Coronavirus cases" cases={123} total={2000} />
-        <InfoBox title="Recovered" total={3000} cases={1256} />
-        <InfoBox title="Deaths" cases={3679} total={30} />
-      </div>
-      <Map />
     </div>
   );
 }
