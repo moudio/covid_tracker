631f5f9a62166179b23b5f8aa26247b4f0140c61
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-01 10:16:39 +0000

    Render the chart on page load

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,19 +1,97 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { useEffect, useState } from 'react';
+import numeral from 'numeral';
 
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: 'index',
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format('+0.0');
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: 'time',
+        time: {
+          format: 'MM/DD/YY',
+          tooltipFormat: 'll',
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format('0a');
+          },
+        },
+      },
+    ],
+  },
+};
 function LineGraph() {
   const [data, setData] = useState({});
   useEffect(() => {
-    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+    const fetchData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then((response) => response.json())
+        .then((data) => {
+          const chartData = buildChartData(data, 'cases');
+          setData(chartData);
+        });
+    };
+    fetchData();
   }, []);
+
+  const buildChartData = (data, casesType = 'cases') => {
+    const chartData = [];
+    let lastDataPoint;
+
+    for (let date in data.cases) {
+      if (lastDataPoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint,
+        };
+        chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[casesType][date];
+    }
+    return chartData;
+  };
   return (
     <div>
-      <Line />
+      {data.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: 'rgba(205,16,52, 0.6)',
+                borderColor: '#CC1035',
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 }
